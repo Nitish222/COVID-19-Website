476671bef9ddf1f6caf6efd54c5ae36eb7dc53d3
Piyush Sapra <60300030+Piyush-00@users.noreply.github.com>
2020-08-28 11:43:21 +0530

    Add files via upload

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: gunicorn covid:app
+
---
+++ covid.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template("page.html")
+
+@app.route('/map/')
+def main():
+    return render_template("Map1.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
